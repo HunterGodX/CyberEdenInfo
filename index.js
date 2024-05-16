b8434f3bb2f5080cd8e627a22c95553607f6c5b2
HunterBot <141238857+HunterGodX@users.noreply.github.com>
2024-05-16 09:51:31 +0600

    Update index.js

--- index.js
+++ index.js
@@ -30,9 +30,9 @@ client.on('ready', async () => {
     .setName('in 💥🅲🆈🅱🅴🆁 🅴🅳🅴🅽✨')
     .setDetails(`@𝟑𝟐𝐊[${formatTime()}]`)
     .setStartTimestamp(Date.now())
- .setAssetsLargeImage('https://cdn.discordapp.com/attachments/1016845411895488532/1142051842126262312/cybereden-cyber.gif?ex=66225029&is=660fdb29&hm=9f499651e8e277aee78a239bb39cad186a84479a79090ce9ffad7778db169dcd&')
+ .setAssetsLargeImage('https://i.postimg.cc/SxqpT5bv/LogoA.gif')
     .setAssetsLargeText('Join in 💥🅲🆈🅱🅴🆁 🅴🅳🅴🅽✨')
-    .setAssetsSmallImage('https://cdn.discordapp.com/attachments/1016845411895488532/1142051842499563591/verified-verificado.gif?ex=66225029&is=660fdb29&hm=57889a17871c1defafe25ea309822b06c82dce9bd9666cedc9a61691f7b03767&')
+    .setAssetsSmallImage('https://i.postimg.cc/rwMw0jg5/Verify-Blue.gif')
     .setAssetsSmallText('𝑽𝒆𝒓𝒊𝒇𝒊𝒆𝒅')
     .addButton('Join the Discord Server', 'https://discord.gg/invite/muamGJmVju')
     .addButton('Add me on Facebook', 'https://www.facebook.com/ritwik.rahman1');
